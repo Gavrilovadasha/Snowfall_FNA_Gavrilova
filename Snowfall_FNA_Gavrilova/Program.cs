72587c5eb6c651d5e2eec3e6b22bcb417f1dc3f9
Gavrilovadasha <gavrilovadasha239@gmail.com>
2024-10-21 06:37:39 +0300

    выполнена работа из видео

---
+++ Snowfall_FNA_Gavrilova/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowfall_FNA_Gavrilova
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (ScreenSaver screensaver = new ScreenSaver())
+            {
+                screensaver.Run();
+            }
+        }
+    }
+}
---
+++ Snowfall_FNA_Gavrilova/Utilites/Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Snowfall_FNA_Gavrilova;
+
+namespace FNAClassCode
+{
+    static class Snowfall_FNA_Gavrilova
+    {
+        public static ScreenSaver game;
+        public static Random random = new Random();
+        public static string levelName;
+
+        public static void Initialize(ScreenSaver inputGame)
+        {
+            game = inputGame;
+        }
+    }
+}
